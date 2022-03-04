8ca2b38de0956a8877ba4ebe9cee6c96a978ff78
Demetrio Turchetta <Kensir96@gmail.com>
2022-03-04 20:01:17 +0100

    push

--- src/components/LeadingOrganisations.vue
+++ src/components/LeadingOrganisations.vue
@@ -1,7 +1,32 @@
 <template>
 <main>
     <div class="container-fluid">
-        culetto
+        <div class="row justify-content-evenly">
+            <div class="col-2">
+                <div class="card ms_card" style="width: 18rem;">
+                    <img src="../assets/images/case-studies-1-1200x1349.jpg" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <p class="card-text ms_card_text">How Spaces attracted five million visitors by improving the content</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="card ms_card" style="width: 18rem;">
+                    <img src="../assets/images/case-studies-4-1200x1349.jpg" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <p class="card-text ms_card_text">Creativity helped Hemisferio to increase their brand reach vertically</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="card ms_card" style="width: 18rem;">
+                    <img src="../assets/images/case-studies-6-1200x1349.jpg" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <p class="card-text ms_card_text">How DigitalBox used AI-powered data insight to boost sales</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </main>
 </template>
@@ -16,5 +41,24 @@ export default {
 <style scoped lang="scss">
 @import '../style/generali.scss';
 
+main {
+
+    .ms_card {
+        border: none;
+        background-color: #f86011;
+
+
+        .ms_card_text {
+            width: 225px;
+            font-size: 17px;
+        }
+
+        img  {
+            -webkit-mask-image: linear-gradient(black, transparent);
+            mask-image: linear-gradient(black, transparent);
+        }
+    }
+
+}
 
 </style>
